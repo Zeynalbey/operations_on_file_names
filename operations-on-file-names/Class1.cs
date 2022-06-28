5fb0b9aa2914dd1d0bc6349922eff2a261821597
Zeynalbey <zeynaleg@code.edu.az>
2022-06-28 16:50:16 +0400

    changes for valudation

---
+++ operations-on-file-names/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operations_on_file_names
+{
+    internal class ValidationForFiles
+    {
+        private string _language;
+        private string _fileName;
+
+        public ValidationForFiles(string language, string fileName)
+        {
+            _language = language;
+            _fileName = fileName;
+        }
+
+        public string FindLanguage(string text)
+        {
+            text = _language;
+
+            if (text == "English")
+            {
+                return "This file ends with: ";
+            }
+            else if (text == "Russian")
+            {
+                return "Этот файл заканчивается следующим образом: ";
+            }
+            else if (text == "Azerbaijani")
+            {
+                return "Bu faylin sonlugu: ";
+            }
+
+            return "Dili duzgun secmesez de, neticeni deyirem:))))";
+        }
+
+        public string SplitToExtention(string text)
+        {
+            text = _fileName;
+
+            string newtext = "";
+            for (int i = text.Length - 1; i > 0; i--)
+            {
+                if (text[i] == '.')
+                {
+                    for (int j = i; j < text.Length; j++)
+                    {
+                        newtext += text[j];
+                    }
+                    break;
+                }
+            }
+            return newtext;
+        }
+
+        public string CheckExtention(string text)
+        {
+            if (SplitToExtention(text) != ".jpg" && SplitToExtention(text) != ".mp3" && SplitToExtention(text) != ".pdf")
+            {
+                return "Faylin uzantisini duzgun yazin. ";
+            }
+            return "";
+        }
+    }
+}
---
+++ operations-on-file-names/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace operations_on_file_names
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Mumkun olan file uzantilari:  .pdf , .mp3 , .jpg");
+                Console.WriteLine("");
+                Console.WriteLine("Menyunun hansi dilde olmasini isteyirsiniz?");
+                Console.WriteLine("1. English");
+                Console.WriteLine("2. Russian");
+                Console.WriteLine("3. Azerbaijani");
+                Console.WriteLine("");
+                string command = Console.ReadLine();
+                Console.WriteLine("");
+                Console.WriteLine("Faylin adini ve uzantisini qeyd edin:");
+                Console.WriteLine("");
+                string command1 = Console.ReadLine();
+                Console.WriteLine("");
+                ValidationForFiles classForValudation = new ValidationForFiles(command, command1);
+                Console.WriteLine(classForValudation.CheckExtention(command1));
+                Console.Write(classForValudation.FindLanguage(command));
+                Console.WriteLine(classForValudation.SplitToExtention(command1));
+                Console.WriteLine("=====================================================================");
+                Console.WriteLine();
+            }
+        }
+    }
+}
